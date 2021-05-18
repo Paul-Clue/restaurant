88236c76d9ff90dd273ef8492b0469488206d127
unknown <paulclue20@gmail.com>
2021-05-18 03:03:02 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//console.log("Is everything working?");
+import _ from 'lodash';
+import './style.css';
+import Img from './food.jpg';
+
+// Add the image to our existing div.
+const myIcon = new Image();
+myIcon.src = Icon;
+
+element.appendChild(myIcon);
